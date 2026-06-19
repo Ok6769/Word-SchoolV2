87b32abc14496d982503a8eeb877c4d89ed979d9
Ok <glebgaming228228@gmail.com>
2026-06-19 09:48:46 +0200

    questions.js

--- questions.js
+++ questions.js
@@ -1,22 +1,27 @@
 const questions = [
-    ["das Kino","le cinéma"],
-    ["die Spinne","une araignée"],
-    ["der Feind","un ennemi"],
-    ["flink","agile"],
-    ["zuverlässig","fiable"],
-    ["der Ritter","le chevalier"],
-    ["mutig","courageux"],
-    ["der Mut","le courage"],
-    ["ehrlich","honnête"],
-    ["grausam","cruel"],
-    ["böse","méchant"],
-    ["die Hexe","la sorcière"],
-    ["eifersüchtig","jaloux"],
-    ["der Vampir","le vampire"],
-    ["unsterblich","immortel"],
-    ["ehrgeizig","ambitieux"],
-    ["der Held","le héros"],
-    ["riesig","géant"],
-    ["schön","beau"],
-    ["gewalttätig","violent"]
+    // Unit 1
+    { de: "das Kino", fr: "le cinéma", unit: 1 },
+    { de: "die Spinne", fr: "une araignée", unit: 1 },
+    { de: "der Feind", fr: "un ennemi", unit: 1 },
+    { de: "flink", fr: "agile", unit: 1 },
+    { de: "zuverlässig", fr: "fiable", unit: 1 },
+    { de: "der Ritter", fr: "le chevalier", unit: 1 },
+
+    // Unit 2
+    { de: "mutig", fr: "courageux", unit: 2 },
+    { de: "der Mut", fr: "le courage", unit: 2 },
+    { de: "ehrlich", fr: "honnête", unit: 2 },
+    { de: "grausam", fr: "cruel", unit: 2 },
+    { de: "böse", fr: "méchant", unit: 2 },
+    { de: "die Hexe", fr: "la sorcière", unit: 2 },
+    { de: "eifersüchtig", fr: "jaloux", unit: 2 },
+
+    // Unit 3
+    { de: "der Vampir", fr: "le vampire", unit: 3 },
+    { de: "unsterblich", fr: "immortel", unit: 3 },
+    { de: "ehrgeizig", fr: "ambitieux", unit: 3 },
+    { de: "der Held", fr: "le héros", unit: 3 },
+    { de: "riesig", fr: "géant", unit: 3 },
+    { de: "schön", fr: "beau", unit: 3 },
+    { de: "gewalttätig", fr: "violent", unit: 3 }
 ];
